0c2f03e1852fadfe228a7c81d5978b218aa9ab4e
KinhaNisha <nishakinha1411@gmail.com>
2024-06-25 02:34:30 +0530

    authHeader

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
+  const authHeader = req.headers['Authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
   if (token == null) return res.sendStatus(401);
